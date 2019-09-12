aeca2e16a96db38ffad096f575ec5955b108e105
Mohan Liu <Mohan.Liu@careerbuilder.com>
2019-09-12 19:01:31 +0800

    temp save

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Sunburst from './graph/sunburst/Sunburst';
+
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +20,8 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Sunburst/>
     </div>
   );
 }
---
+++ src/graph/sunburst/Sunburst.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import d3 from 'd3';
+
+const Sunburst: React.FC = () => {
+  return (
+    <div id="sunburst">
+    </div>
+  )
+}
+
+export default Sunburst;
